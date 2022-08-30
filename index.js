d1d56fbe3b3d92ad86329db5394a70cfabf5ca87
Michaela Varberg <michaelavarberg@gmail.com>
2022-08-29 18:18:48 -0600

    created functions to ask engineer and intern prompts

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const generatePage = require("./generatePage");
 const inquirer = require("inquirer");
 const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 let employeesArray = [];
+
 function init() {
   inquirer
     .prompt([
@@ -52,8 +55,90 @@ function init() {
     });
 }
 
-function addEngineerPrompts() {}
+function addEngineerPrompts() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is this engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is this engineer's id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is this engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is this engineer's github username?",
+        name: "extra",
+      },
+      {
+        type: "list",
+        message: "What would you like to do next?",
+        name: "next",
+        choices: ["Add an Engineer", "Add an intern", "Finalize my page"],
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(response);
+      employeesArray.push(engineer);
+      if (response.next === "Finalize my page") {
+        generatePage(employeesArray);
+      } else if (response.next === "Add an Engineer") {
+        addEngineerPrompts();
+      } else {
+        addInternPrompts();
+      }
+    });
+}
 
-function addInternPrompts() {}
+function addInternPrompts() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is this intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is this intern's id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is this intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Where does this intern attend school?",
+        name: "extra",
+      },
+      {
+        type: "list",
+        message: "What would you like to do next?",
+        name: "next",
+        choices: ["Add an Engineer", "Add an intern", "Finalize my page"],
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(response);
+      employeesArray.push(intern);
+      if (response.next === "Finalize my page") {
+        generatePage(employeesArray);
+      } else if (response.next === "Add an Engineer") {
+        addEngineerPrompts();
+      } else {
+        addInternPrompts();
+      }
+    });
+}
 
 init();
